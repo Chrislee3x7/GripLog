b0f59bc35bda0b5a41fa0229117143ddef0307bd
Chrislee3x7 <32285657+Chrislee3x7@users.noreply.github.com>
2023-10-23 23:46:42 -0400

    added more basic backend, post and get requests work to mongoAtlas through postman

--- server/app.js
+++ server/app.js
@@ -2,9 +2,27 @@ const express = require('express');
 const app = express();
 // for parsing json bodies
 const bodyParser = require('body-parser');
+// middleware library
+// helps log requests (GET api/v1/problems etc...) to the server console
+const morgan = require('morgan');
+// helps to connect to mongoDB
+const mongoose = require('mongoose');
 
 //middleware
 app.use(bodyParser.json());
+app.use(morgan('tiny'));
+
+const problemSchema = mongoose.Schema({
+  name: String,
+  grade: Number,
+  image: String,
+  attemptCount: {
+    type: Number,
+    required: true
+  }
+});
+
+const Problem = mongoose.model('Problem', problemSchema);
 
 // for access to .env file variables
 // any variables defined in .env are accessible anywhere in the
@@ -16,20 +34,42 @@ const api = process.env.API_URL;
 // Initial route that a Get request will take.
 // Visitinghttp://localhost:3000 will yield "hello API!"
 // http://localhost:3000/api/v1/problems
-app.get(`${api}/problems`, (req, res) => {
-  const problem = {
-    id: 1,
-    name: 'Inevitable',
-    grade: 'v6',
-    image: 'some_url'
+app.get(`${api}/problems`, async (req, res) => {
+  // gets all Problems
+  const problemList = await Problem.find();
+  if (!problemList) {
+    res.status(500).json({succes: false});
   }
-  res.send(problem);
+  res.send(problemList);
 });
 
-app.post(`${api}/problems`, (req, res) => {
-  const newProblem = req.body;
-  console.log(newProblem);
-  res.send(newProblem);
+app.post(`${api}/problems`, async (req, res) => {
+  const problem = new Problem({
+    name: req.body.name,
+    grade: req.body.grade,
+    image: req.body.image,
+    attemptCount: req.body.attemptCount
+  });
+
+  const createdProblem = await problem.save();
+  if (!createdProblem) {
+    res.status(500).json({
+      error: err,
+      success: false
+    });
+  } else {
+    res.status(201).json(createdProblem);
+  }
+});
+
+// add connection before starting server
+mongoose.connect(process.env.CONNECTION_STRING, {
+  // 'griplogdb' lines up with database name on MongoAtlas
+  dbName: 'griplogdb'
+}).then(() => {
+  console.log('Database Connection is ready...');
+}).catch((err) => {
+  console.log(err);
 });
 
 app.listen(3000, () => {
